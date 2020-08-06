b8d4154fec6d3661fab72509383a42407660f562
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-05 22:18:22 -0400

    Updates

--- server/index.js
+++ server/index.js
@@ -92,6 +92,6 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000
 
-app.listen(process.env.PORT, () => {
+app.listen(port, () => {
   console.log(`Server Listening on ${port}`)
 });
