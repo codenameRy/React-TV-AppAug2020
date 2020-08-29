69c84ad0702d956709beaf01ad3f8b2469113e54
Ryan Lee <ryanm.lee11@gmail.com>
2020-08-28 22:18:27 -0400

    Styling updates

--- client/src/components/views/TVShowDetails/GridDesignShows.js
+++ client/src/components/views/TVShowDetails/GridDesignShows.js
@@ -4,9 +4,9 @@ import {Col} from 'antd';
 function GridDesignShows(props) {
     if (props.actor) {
         return (
-            <Col lg={6} md={8} sm={16} xs={24}>
+            <Col lg={6} md={8} sm={16} xs={32}>
                  <div style={{ position: 'relative', justifyContent: 'center'}}>
-                        <img style={{ width: '100%', height: '320px' }} alt={'temp'} src={props.image} />
+                        <img style={{ width: '100%', height: '300px' }} alt={'temp'} src={props.image} />
                         <h3 style={{display: 'inline-block'}}>{props.actorname}</h3>
                 </div>
             </Col>
